3c66c7123838b834c7ad4b50150c1329b2b0f4c0
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-08-10 16:49:44 +0200

    Create dataBinding base adapter

---
+++ app/src/main/java/danilchanka/aliaksandr/contacts/adapter/base/BaseAdapter.kt
@@ -0,0 +1,47 @@
+package danilchanka.aliaksandr.contacts.adapter.base
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+abstract class BaseAdapter<E> : RecyclerView.Adapter<BaseAdapter.BindingHolder>() {
+
+    private var mElements: List<E>? = null
+
+    abstract val itemLayoutId: Int
+    abstract val variableId: Int
+
+    init {
+        mElements = ArrayList()
+    }
+
+    fun setElements(elements: List<E>) {
+        mElements = elements
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
+        val v = LayoutInflater.from(parent.context).inflate(itemLayoutId,
+                parent, false)
+        return BindingHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: BindingHolder, position: Int) {
+        val element = mElements!![position]
+        holder.binding!!.setVariable(variableId, element)
+        holder.binding.executePendingBindings()
+    }
+
+    override fun getItemCount(): Int {
+        return mElements!!.size
+    }
+
+    class BindingHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val binding: ViewDataBinding? = DataBindingUtil.bind(itemView)
+
+    }
+}
