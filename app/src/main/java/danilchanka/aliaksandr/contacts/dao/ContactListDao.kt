7c3a6bdfe56b9cedfb19d26f6a3cbb031cfb895c
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-08-10 12:33:04 +0200

    Delete sort by name

--- app/src/main/java/danilchanka/aliaksandr/contacts/dao/ContactListDao.kt
+++ app/src/main/java/danilchanka/aliaksandr/contacts/dao/ContactListDao.kt
@@ -15,6 +15,6 @@ interface ContactListDao {
     @Query("DELETE FROM contact_list_table")
     fun deleteAll()
 
-    @Query("SELECT * from contact_list_table ORDER BY name ASC")
+    @Query("SELECT * from contact_list_table")
     fun getContactList(): Flowable<List<Contact>>
 }
