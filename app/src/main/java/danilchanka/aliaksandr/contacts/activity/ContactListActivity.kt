362bddfc8f93df329805a1fa56f5fda4b9cdd2ad
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-09-01 21:21:02 +0200

    Add base base file and base functions

--- app/src/main/java/danilchanka/aliaksandr/contacts/activity/ContactListActivity.kt
+++ app/src/main/java/danilchanka/aliaksandr/contacts/activity/ContactListActivity.kt
@@ -1,21 +1,26 @@
 package danilchanka.aliaksandr.contacts.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import danilchanka.aliaksandr.contacts.R
+import danilchanka.aliaksandr.contacts.activity.base.BaseFragmentActivity
 import danilchanka.aliaksandr.contacts.fragment.ContactListFragment
+import kotlinx.android.synthetic.main.activity_contact_list.*
 
-class ContactListActivity : AppCompatActivity() {
+class ContactListActivity : BaseFragmentActivity() {
+
+    override fun getFragmentContainerId(): Int = R.id.content
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_list)
+        setUpAppBar()
 
         if (savedInstanceState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.content, ContactListFragment.newInstance())
-                    .commit()
+            addFragment(ContactListFragment.newInstance())
         }
+    }
+
+    private fun setUpAppBar() {
+        setSupportActionBar(toolbar)
     }
 }
