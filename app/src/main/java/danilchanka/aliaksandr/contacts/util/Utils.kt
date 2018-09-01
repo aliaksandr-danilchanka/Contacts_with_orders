7c674f06d54e495699f4e334ce5d83cfca94b8d2
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-09-01 21:29:57 +0200

    Add const value

--- app/src/main/java/danilchanka/aliaksandr/contacts/util/Utils.kt
+++ app/src/main/java/danilchanka/aliaksandr/contacts/util/Utils.kt
@@ -4,7 +4,8 @@ class Utils {
 
     companion object {
 
-        val CONTACT_PICTURE_URL_PREFIX = "https://inloop-contacts.appspot.com/"
+        private const val CONTACT_PICTURE_URL_PREFIX = "https://inloop-contacts.appspot.com/"
+        const val BASE_URL = "https://inloop-contacts.appspot.com/_ah/api/"
 
         fun getContactPictureUrl(pictureUrl: String?): String {
             return CONTACT_PICTURE_URL_PREFIX + pictureUrl
