de4dad653a25fadf4d841dfdb23a0fa461404359
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-09-01 21:19:39 +0200

    Renamed and replaced ImageBinder.kt file

--- app/src/main/java/danilchanka/aliaksandr/contacts/util/BindingAdapterUtils.kt
+++ app/src/main/java/danilchanka/aliaksandr/contacts/util/BindingAdapterUtils.kt
@@ -1,13 +1,12 @@
-package danilchanka.aliaksandr.contacts.adapter.binder
+package danilchanka.aliaksandr.contacts.util
 
 import android.databinding.BindingAdapter
+import android.view.View
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import danilchanka.aliaksandr.contacts.R
-import danilchanka.aliaksandr.contacts.util.Utils
 
-
-object ImageBinder {
+object BindingAdapterUtils {
 
     @BindingAdapter("imageUrl")
     @JvmStatic
@@ -17,5 +16,17 @@ object ImageBinder {
                 .load(Utils.getContactPictureUrl(url))
                 .placeholder(R.drawable.contact_icon)
                 .into(imageView)
+    }
+
+    @BindingAdapter("hide")
+    @JvmStatic
+    fun setHide(view: View, hide: Boolean) {
+        view.visibility = if (hide) View.GONE else View.VISIBLE
+    }
+
+    @BindingAdapter("show")
+    @JvmStatic
+    fun setShow(view: View, show: Boolean) {
+        view.visibility = if (show) View.VISIBLE else View.GONE
     }
 }
