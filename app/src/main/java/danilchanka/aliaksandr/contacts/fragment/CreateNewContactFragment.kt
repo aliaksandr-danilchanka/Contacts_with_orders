52bb399635995c3ca451f686785b5077e1b93e03
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-09-01 21:22:25 +0200

    Delete ";"

--- app/src/main/java/danilchanka/aliaksandr/contacts/fragment/CreateNewContactFragment.kt
+++ app/src/main/java/danilchanka/aliaksandr/contacts/fragment/CreateNewContactFragment.kt
@@ -14,6 +14,6 @@ class CreateNewContactFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_create_new_contact, container, false);
+        return inflater.inflate(R.layout.fragment_create_new_contact, container, false)
     }
 }
