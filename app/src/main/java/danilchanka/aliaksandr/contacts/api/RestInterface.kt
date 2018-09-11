299c8b54217cd4d5985b2aa98c166af7096558ab
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-09-11 14:55:18 +0200

    Add POST function

--- app/src/main/java/danilchanka/aliaksandr/contacts/api/RestInterface.kt
+++ app/src/main/java/danilchanka/aliaksandr/contacts/api/RestInterface.kt
@@ -1,12 +1,18 @@
 package danilchanka.aliaksandr.contacts.api
 
+import danilchanka.aliaksandr.contacts.model.Contact
 import danilchanka.aliaksandr.contacts.model.ContactListResponse
+import danilchanka.aliaksandr.contacts.model.CreateContactRequest
 import io.reactivex.Observable
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface RestInterface {
 
     @GET("contactendpoint/v1/contact")
     fun getContactList(): Observable<ContactListResponse>
 
+    @POST("contactendpoint/v1/contact")
+    fun createNewContact(@Body request: CreateContactRequest): Observable<Contact>
 }
