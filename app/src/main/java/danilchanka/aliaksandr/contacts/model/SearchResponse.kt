bec5f3d2c2a3ba56389280371c2661a342c95db6
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-09-28 01:55:25 +0200

    Add Parcelable implementation

--- app/src/main/java/danilchanka/aliaksandr/contacts/model/SearchResponse.kt
+++ app/src/main/java/danilchanka/aliaksandr/contacts/model/SearchResponse.kt
@@ -3,6 +3,8 @@ package danilchanka.aliaksandr.contacts.model
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.os.Parcel
+import android.os.Parcelable
 import android.support.annotation.NonNull
 import com.google.gson.annotations.SerializedName
 
@@ -23,6 +25,33 @@ data class Contact(
 
         @ColumnInfo(name = "pictureUrl")
         @SerializedName("pictureUrl") val pictureUrl: String?
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeString(phone)
+        parcel.writeString(pictureUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Contact> {
+        override fun createFromParcel(parcel: Parcel): Contact {
+            return Contact(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Contact?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
 data class ContactListResponse(@SerializedName("items") val items: ArrayList<Contact>)
